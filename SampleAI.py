0bd54e3b2215ae17d84cfc5cb8bc9a73ab64cfee
GMago123 <1326742692@qq.com>
2020-07-08 19:29:11 +0800

    更新简单策略

--- SampleAI.py
+++ SampleAI.py
@@ -13,6 +13,7 @@ obj.connect((host, port))
 shotnum = str("0")
 order = str("Player1")   # 先后手
 state = []
+MANUAL = False
 
 
 # 策略
@@ -26,56 +27,6 @@ def handle_strategy():
     v, h_x, w = map(float, input().split())
     bestshot = list_to_str([v, h_x, w])
     return bestshot
-
-
-def sample_strategy(state_list, order):
-    res = []
-    sorted_res = []
-    i = 0
-    while i < 30:
-        if is_in_house(float(state_list[0][i]), float(state_list[0][i+1])):
-            res.append(i)
-        i += 2
-    # 大本营内没有球，向大本营中心打球
-    if not res:
-        bestshot = str("BESTSHOT 3.05 1.65 -10")
-        return bestshot
-    # 大本营有球
-    else:
-        for i in res:
-            sorted_res.append([get_dist(float(state_list[0][i]), float(state_list[0][i+1])), i])
-            sorted_res = sorted(sorted_res)
-        # 先手
-        if order == str("Player1"):
-            # 离大本营中心最近的球是自己的，防守
-            if sorted_res[0][1] % 4 == 0:
-                # print("防守球")
-                target = [float(state_list[0][sorted_res[0][1]]), float(state_list[0][sorted_res[0][1]+1])+2]
-                v = float(3.613-0.12234*target[1])
-                h_x = float(target[0]-2.375)
-            # 大本营离中心最近的球是自对方的，撞飞
-            else:
-                # print("撞飞")
-                target = [float(state_list[0][sorted_res[0][1]]), float(state_list[0][sorted_res[0][1]+1])]
-                v = float(3.613-0.12234*target[1]+1)
-                h_x = float(target[0]-2.375)
-        # 后手
-        else:
-            if sorted_res[0][1] % 4 == 2:
-                # print("防守球")
-                target = [float(state_list[0][sorted_res[0][1]]), float(state_list[0][sorted_res[0][1]+1])+2]
-                v = float(3.613-0.12234*target[1])
-                h_x = float(target[0]-2.375)
-            # 大本营离中心最近的球是自对方的，撞飞
-            else:
-                # print("撞飞")
-                target = [float(state_list[0][sorted_res[0][1]]), float(state_list[0][sorted_res[0][1]+1])]
-                v = float(3.613-0.12234*target[1]+1)
-                h_x = float(target[0]-2.375)
-        
-        bestshot = [v, h_x, 0]
-        bestshot = list_to_str(bestshot)
-        return bestshot
 
 
 while True:
@@ -103,7 +54,10 @@ while True:
         state.append(shotnum)
         print(state)
     if messageList[0] == "GO":
-        shot = strategy(state, order)
+        if MANUAL:
+            shot = handle_strategy()
+        else:
+            shot = strategy(state, order)
         obj.send(bytes(shot, encoding="utf-8"))
     if messageList[0] == "MOTIONINFO":
         x_coordinate = float(messageList[1])
--- Strategy.py
+++ Strategy.py
@@ -31,6 +31,7 @@ class Curling:
         unknown: 未知
         score: 大本营里的得分球
         non_score: 大本营里的非得分球
+        camp: 位于大本营里的球
         defend: 自由防守区的防守球
         """
 
@@ -43,15 +44,16 @@ class Curling:
     def is_mine(self):
         return self.my
 
-    def use(self):
-        pass
+    def mark(self, state):
+        self.marker = state
 
 
 class Parser:         # 场景解析
     def __init__(self, state_list, order):
         self.positions = state_list[0]
-        self.current_shotnum = state_list[1]
+        self.current_shotnum = int(state_list[1])
         self.player = order
+        self.curlings = None
 
     def base_parse(self):
         curlings = []
@@ -73,9 +75,17 @@ class Parser:         # 场景解析
             curlings.append(curling_i)
             curlings = sorted(curlings)
 
+        for curling in curlings:
+            if curling.is_in_house():
+                curling.mark('non_score')          # 大本营内先统一标注为未得分球
+            else:
+                curling.mark('defend')
+
+        self.curlings = curlings
+
         return curlings
 
-    def score(self):
+    def score(self):        # 分析得分球
         score = [0, 0]  # index0: my_score  index1: enemy_score
         curlings = self.base_parse()
 
@@ -85,17 +95,77 @@ class Parser:         # 场景解析
                 index = 0 if curling.is_mine() else 1
                 if index == nonzero_score_index:
                     score[index] += 1
+                    curling.mark('score')
                 else:
                     break
 
         return score
+
+    def curlings_statistics(self):         # 统计各个类型的冰壶数量
+        my_curlings = {'score': 0, 'non_score': 0, 'defend': 0}
+        enemy_curlings = {'score': 0, 'non_score': 0, 'defend': 0}
+        for curling in self.curlings:
+            if curling.is_mine():
+                if curling.marker in my_curlings.keys():
+                    my_curlings[curling.marker] += 1
+                else:
+                    my_curlings[curling.marker] = 1
+            else:
+                if curling.marker in enemy_curlings.keys():
+                    enemy_curlings[curling.marker] += 1
+                else:
+                    enemy_curlings[curling.marker] = 1
+        return my_curlings, enemy_curlings
+
+    def find_curling(self, my_flag, marker):
+        for curling in self.curlings:
+            if curling.is_mine() == my_flag and marker == curling.marker:
+                return curling
+
+        return None
+
+    def have_defend(self, curling):
+        for other_curling in self.curlings:
+            if other_curling.is_in_house():
+                continue
+            offset = abs(float(curling.x) - float(other_curling.x))
+            if offset < 0.2:
+                print('Current score curling have defend.')
+                return True
+        return False
+
+
+class Throw:
+    @staticmethod
+    def center():
+        return 3.05, 1.65, -10
+
+    @staticmethod
+    def defense(curling):
+        return 2.45, float(curling.x - 2.375), 0
+
+    @staticmethod
+    def defense2(curling):
+        return 2.45, float(curling.x - 2.375 + 0.5), 0
+
+    @staticmethod
+    def free_defense_center_1():
+        return 2.5, -0.8, 0
+
+    @staticmethod
+    def free_defense_center_2():
+        return 2.5, 0, 0
+
+    @staticmethod
+    def hit(curling):
+        return float(3.613 - 0.12234 * curling.y + 1), float(curling.x - 2.375), 0
 
 
 class Decision:
     def __init__(self, parser):
         self.parser = parser
 
-    def decide(self):
+    def raw_decide(self):
         curlings = self.parser.base_parse()
         if not curlings:
             v = 3.05
@@ -111,7 +181,59 @@ class Decision:
                 h_x = float(curlings[0].x - 2.375)
                 w = 0
 
-        bestshot = [v, h_x, 0]
+        bestshot = [v, h_x, w]
         bestshot = list_to_str(bestshot)
         return bestshot
 
+    def decide(self):
+        self.parser.base_parse()
+        current_score = self.parser.score()
+        my_curlings_num, enemy_curlings_num = self.parser.curlings_statistics()
+        shotnum = self.parser.current_shotnum
+        protect_flag = (shotnum < 5)   # 自由防守区生效flag
+        offensive_flag = (shotnum % 2 == 0)   # 先手flag
+        final_flag = shotnum >= 14     # 最后一球flag
+
+        if protect_flag and offensive_flag:   # 前5球，2球占位
+            if shotnum / 2 == 0:
+                v, h_x, w = Throw.free_defense_center_1()
+            elif shotnum / 2 == 1:
+                v, h_x, w = Throw.free_defense_center_2()
+            else:
+                v, h_x, w = Throw.center()
+
+        elif protect_flag and not offensive_flag:  # 前5球，2球占位
+            if shotnum / 2 == 0:
+                v, h_x, w = Throw.free_defense_center_1()
+            elif shotnum / 2 == 1:
+                v, h_x, w = Throw.free_defense_center_2()
+            else:
+                v, h_x, w = Throw.center()
+
+        elif final_flag:
+            if offensive_flag:
+                v, h_x, w = Throw.center()
+            else:
+                v, h_x, w = Throw.center()
+
+        elif enemy_curlings_num['defend'] > my_curlings_num['defend'] and my_curlings_num['non_score'] > 0:
+            target = self.parser.find_curling(False, 'defend')
+            v, h_x, w = Throw.hit(target)
+        elif enemy_curlings_num['score'] > 1:
+            target = self.parser.find_curling(False, 'score')
+            v, h_x, w = Throw.hit(target)
+        elif enemy_curlings_num['non_score'] > 1:
+            target = self.parser.find_curling(False, 'non_score')
+            v, h_x, w = Throw.hit(target)
+        elif current_score[0] != 0:
+            target = self.parser.find_curling(True, 'score')
+            if self.parser.have_defend(target):
+                v, h_x, w = Throw.center()
+            else:
+                v, h_x, w = Throw.defense(target)
+        else:
+            v, h_x, w = Throw.center()
+
+        bestshot = [v, h_x, w]
+        bestshot = list_to_str(bestshot)
+        return bestshot
