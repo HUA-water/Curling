54dd3c12de611701c6b3224f63edb4b1c31e3763
suikasxt <suikasxt@gmail.com>
2020-07-14 01:05:45 +0800

    DDPG

---
+++ DDPG.py
@@ -0,0 +1,301 @@
+"""
+Deep Deterministic Policy Gradient (DDPG)
+-----------------------------------------
+An algorithm concurrently learns a Q-function and a policy.
+It uses off-policy data and the Bellman equation to learn the Q-function,
+and uses the Q-function to learn the policy.
+Reference
+---------
+Deterministic Policy Gradient Algorithms, Silver et al. 2014
+Continuous Control With Deep Reinforcement Learning, Lillicrap et al. 2016
+MorvanZhou's tutorial page: https://morvanzhou.github.io/tutorials/
+Environment
+-----------
+Openai Gym Pendulum-v0, continual action space
+Prerequisites
+-------------
+tensorflow >=2.0.0a0
+tensorflow-probability 0.6.0
+tensorlayer >=2.0.0
+To run
+------
+python tutorial_DDPG.py --train/test
+"""
+
+import argparse
+import os
+import time
+
+import env
+import gym
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+import tensorlayer as tl
+
+parser = argparse.ArgumentParser(description='Train or test neural net motor controller.')
+parser.add_argument('--train', dest='train', action='store_true', default=True)
+parser.add_argument('--test', dest='test', action='store_false')
+args = parser.parse_args()
+
+#####################  hyper parameters  ####################
+
+ENV_NAME = 'Pendulum-v0'	# environment name
+RANDOMSEED = 1			  # random seed
+
+LR_A = 0.001				# learning rate for actor
+LR_C = 0.002				# learning rate for critic
+GAMMA = 0.9				 # reward discount
+TAU = 0.01				  # soft replacement
+MEMORY_CAPACITY = 10000	 # size of replay buffer
+BATCH_SIZE = 16			 # update batchsize
+
+MAX_EPISODES = 200		  # total number of episodes for training
+MAX_EP_STEPS = 4		  # total number of steps for each episode
+TEST_PER_EPISODES = 10	  # test the model per episodes
+VAR = 1					 # control exploration
+
+###############################  DDPG  ####################################
+
+class DDPG(object):
+	"""
+	DDPG class
+	"""
+	def __init__(self, a_dim, s_dim, a_bound, a_mean, name):
+		# memory用于储存跑的数据的数组：
+		# 保存个数MEMORY_CAPACITY，s_dim * 2 + a_dim + 1：分别是两个state，一个action，和一个reward
+		self.memory = np.zeros((MEMORY_CAPACITY, s_dim * 2 + a_dim + 1), dtype=np.float32)
+		self.pointer = 0
+		self.a_dim, self.s_dim, self.a_bound, self.a_mean = a_dim, s_dim, a_bound, a_mean
+		self.name = name
+
+		W_init = tf.random_normal_initializer(mean=0, stddev=0.3)
+		b_init = tf.constant_initializer(0.1)
+
+		# 建立actor网络，输入s，输出a
+		def get_actor(input_state_shape, name=''):
+			"""
+			Build actor network
+			:param input_state_shape: state
+			:param name: name
+			:return: act
+			"""
+			inputs = tl.layers.Input(input_state_shape, name='A_input')
+			x = tl.layers.Dense(n_units=40, act=tf.nn.relu, W_init=W_init, b_init=b_init, name='A_l1')(inputs)
+			x = tl.layers.Dense(n_units=40, act=tf.nn.relu, W_init=W_init, b_init=b_init, name='A_l2')(x)
+			x = tl.layers.Dense(n_units=a_dim, act=tf.nn.tanh, W_init=W_init, b_init=b_init, name='A_a')(x)
+			x = tl.layers.Lambda(lambda x: np.array(a_bound) * x + np.array(a_mean))(x)			#注意这里，先用tanh把范围限定在[-1,1]之间，再进行映射
+			return tl.models.Model(inputs=inputs, outputs=x, name='Actor' + name)
+
+		#建立Critic网络，输入s，a。输出Q值
+		def get_critic(input_state_shape, input_action_shape, name=''):
+			"""
+			Build critic network
+			:param input_state_shape: state
+			:param input_action_shape: act
+			:param name: name
+			:return: Q value Q(s,a)
+			"""
+			s = tl.layers.Input(input_state_shape, name='C_s_input')
+			a = tl.layers.Input(input_action_shape, name='C_a_input')
+			x = tl.layers.Concat(1)([s, a])
+			x = tl.layers.Dense(n_units=60, act=tf.nn.relu, W_init=W_init, b_init=b_init, name='C_l1')(x)
+			x = tl.layers.Dense(n_units=60, act=tf.nn.relu, W_init=W_init, b_init=b_init, name='C_l2')(x)
+			x = tl.layers.Dense(n_units=1, W_init=W_init, b_init=b_init, name='C_out')(x)
+			return tl.models.Model(inputs=[s, a], outputs=x, name='Critic' + name)
+
+		self.actor = get_actor([None, s_dim], name = '_' + self.name)
+		self.critic = get_critic([None, s_dim], [None, a_dim], name = '_' + self.name)
+		self.actor.train()
+		self.critic.train()
+
+		#更新参数，只用于首次赋值，之后就没用了
+		def copy_para(from_model, to_model):
+			"""
+			Copy parameters for soft updating
+			:param from_model: latest model
+			:param to_model: target model
+			:return: None
+			"""
+			for i, j in zip(from_model.trainable_weights, to_model.trainable_weights):
+				j.assign(i)
+
+		#建立actor_target网络，并和actor参数一致，不能训练
+		self.actor_target = get_actor([None, s_dim], name = '_' + self.name + '_target')
+		copy_para(self.actor, self.actor_target)
+		self.actor_target.eval()
+
+		#建立critic_target网络，并和actor参数一致，不能训练
+		self.critic_target = get_critic([None, s_dim], [None, a_dim], name = '_' + self.name + '_target')
+		copy_para(self.critic, self.critic_target)
+		self.critic_target.eval()
+
+		self.R = tl.layers.Input([None, 1], tf.float32, 'r')
+
+		#建立ema，滑动平均值
+		self.ema = tf.train.ExponentialMovingAverage(decay=1 - TAU)  # soft replacement
+
+		self.actor_opt = tf.optimizers.Adam(LR_A)
+		self.critic_opt = tf.optimizers.Adam(LR_C)
+
+
+	def ema_update(self):
+		"""
+		滑动平均更新
+		"""
+		# 其实和之前的硬更新类似，不过在更新赋值之前，用一个ema.average。
+		paras = self.actor.trainable_weights + self.critic.trainable_weights	#获取要更新的参数包括actor和critic的
+		self.ema.apply(paras)												   #主要是建立影子参数
+		for i, j in zip(self.actor_target.trainable_weights + self.critic_target.trainable_weights, paras):
+			i.assign(self.ema.average(j))									   # 用滑动平均赋值
+
+	# 选择动作，把s带进入，输出a
+	def choose_action(self, s):
+		"""
+		Choose action
+		:param s: state
+		:return: act
+		"""
+		return self.actor(np.array([s], dtype=np.float32))[0]
+
+	def learn(self):
+		"""
+		Update parameters
+		:return: None
+		"""
+		indices = np.random.choice(np.min([self.pointer, MEMORY_CAPACITY]), size=BATCH_SIZE)	#随机BATCH_SIZE个随机数
+		#print(indices)
+		bt = self.memory[indices, :]					#根据indices，选取数据bt，相当于随机
+		bs = bt[:, :self.s_dim]						 #从bt获得数据s
+		ba = bt[:, self.s_dim:self.s_dim + self.a_dim]  #从bt获得数据a
+		br = bt[:, -self.s_dim - 1:-self.s_dim]		 #从bt获得数据r
+		bs_ = bt[:, -self.s_dim:]					   #从bt获得数据s'
+
+		# Critic：
+		# Critic更新和DQN很像，不过target不是argmax了，是用critic_target计算出来的。
+		# br + GAMMA * q_
+		with tf.GradientTape() as tape:
+			a_ = self.actor_target(bs_)
+			q_ = self.critic_target([bs_, a_])
+			y = br + GAMMA * q_
+			q = self.critic([bs, ba])
+			td_error = tf.losses.mean_squared_error(y, q)
+		c_grads = tape.gradient(td_error, self.critic.trainable_weights)
+		self.critic_opt.apply_gradients(zip(c_grads, self.critic.trainable_weights))
+
+		# Actor：
+		# Actor的目标就是获取最多Q值的。
+		with tf.GradientTape() as tape:
+			a = self.actor(bs)
+			q = self.critic([bs, a])
+			a_loss = -tf.reduce_mean(q)  # 【敲黑板】：注意这里用负号，是梯度上升！也就是离目标会越来越远的，就是越来越大。
+		a_grads = tape.gradient(a_loss, self.actor.trainable_weights)
+		self.actor_opt.apply_gradients(zip(a_grads, self.actor.trainable_weights))
+
+		self.ema_update()
+
+
+	# 保存s，a，r，s_
+	def store_transition(self, s, a, r, s_):
+		"""
+		Store data in data buffer
+		:param s: state
+		:param a: act
+		:param r: reward
+		:param s_: next state
+		:return: None
+		"""
+		# 整理s，s_,方便直接输入网络计算
+		s = s.astype(np.float32)
+		s_ = s_.astype(np.float32)
+
+		#把s, a, [r], s_横向堆叠
+		transition = np.hstack((s, a, [r], s_))
+
+		#pointer是记录了曾经有多少数据进来。
+		#index是记录当前最新进来的数据位置。
+		#所以是一个循环，当MEMORY_CAPACITY满了以后，index就重新在最底开始了
+		index = self.pointer % MEMORY_CAPACITY  # replace the old memory with new memory
+		#把transition，也就是s, a, [r], s_存进去。
+		self.memory[index, :] = transition
+		self.pointer += 1
+
+	def save_ckpt(self, path):
+		"""
+		save trained weights
+		:return: None
+		"""
+		if not os.path.exists(path):
+			os.makedirs(path)
+
+		tl.files.save_weights_to_hdf5(path + '/ddpg_actor.hdf5', self.actor)
+		tl.files.save_weights_to_hdf5(path + '/ddpg_actor_target.hdf5', self.actor_target)
+		tl.files.save_weights_to_hdf5(path + '/ddpg_critic.hdf5', self.critic)
+		tl.files.save_weights_to_hdf5(path + '/ddpg_critic_target.hdf5', self.critic_target)
+
+	def load_ckpt(self, path):
+		"""
+		load trained weights
+		:return: None
+		"""
+		tl.files.load_hdf5_to_weights_in_order(path + '/ddpg_actor.hdf5', self.actor)
+		tl.files.load_hdf5_to_weights_in_order(path + '/ddpg_actor_target.hdf5', self.actor_target)
+		tl.files.load_hdf5_to_weights_in_order(path + '/ddpg_critic.hdf5', self.critic)
+		tl.files.load_hdf5_to_weights_in_order(path + '/ddpg_critic_target.hdf5', self.critic_target)
+
+
+if __name__ == '__main__':
+	
+	#初始化环境
+	env = env.Env()
+
+	# reproducible，设置随机种子，为了能够重现
+	#env.seed(RANDOMSEED)
+	np.random.seed(RANDOMSEED)
+	tf.random.set_seed(RANDOMSEED)
+
+	#定义状态空间，动作空间，动作幅度范围
+	s_dim = env.observation_space.shape[0]
+	a_dim = env.action_space.shape[0]
+	a_bound = (env.action_space.high - env.action_space.low)/2
+	a_mean = (env.action_space.high + env.action_space.low)/2
+
+	#用DDPG算法，先后手两个智能体
+	ddpg = [DDPG(a_dim, s_dim, a_bound, a_mean, '0'), DDPG(a_dim, s_dim, a_bound, a_mean, '1')]
+
+	for i in range(len(ddpg)):
+		ddpg[i].load_ckpt('model_' + str(i))
+		pass
+		
+	#训练部分：
+	if args.train:  # train
+		for i in range(MAX_EPISODES):
+			t0 = time.time()		#统计时间
+			for j in range(MAX_EP_STEPS):
+				# Add exploration noise
+				s_last = env.reset()
+				a = np.array(ddpg[0].choose_action(s_last))
+				#print(a)
+				a = np.clip(np.random.normal(a, VAR), env.action_space.low, env.action_space.high)  
+				s, _, _, _ = env.step(a)
+				a_last = a
+				for k in range(1,16):
+					a = np.array(ddpg[k&1].choose_action(s))
+					a = np.clip(np.random.normal(a, VAR), env.action_space.low, env.action_space.high)  
+					s_next, reward, done, info = env.step(a)
+					ddpg[(k&1)^1].store_transition(s_last, a_last, -reward, s_next)
+					print(a_last, reward, s_last)
+					s_last = s
+					s = s_next
+					a_last = a
+				ddpg[1].store_transition(s_last, a_last, reward, s_next)
+
+				for i in [0,1]:
+					if ddpg[i].pointer > BATCH_SIZE:
+						for j in range(100):
+							ddpg[i].learn()
+
+			print('\nRunning time: ', time.time() - t0)
+			for i in range(len(ddpg)):
+				ddpg[i].save_ckpt('model_' + str(i))
--- env.py
+++ env.py
@@ -3,16 +3,19 @@ import socket
 import _thread
 import time
 import pyautogui
+import gym
+import numpy as np
 
 #快速模式、准备、开始对局、返回主菜单
 CLICK_POSITION = [[700, 575], [1010, 610], [1010, 710], [50, 150]]
+TEE = [2.375, 4.88]
 SLOW_MODE = 0
 
 host='127.0.0.1'
 port=7788
 
     
-def list_to_str(list):    
+def list_to_str(list):
     res = str(list)[1:-1].replace(',','')
     return res
 class Player:
@@ -20,20 +23,22 @@ class Player:
 		self.Socker = socket.socket()
 		self.Socker.connect((host,port))
 		self.name = name
-		self.state = []
+		self.state = [[0,0]]*16
 		self.x_coordinate = -1
 		self.y_coordinate = -1
 		self.x_velocity = -1
 		self.y_velocity = -1
 		self.angular_velocity = -1
 		self.go = False
+		self.score = -1
+		self.getNewState = False
 		
 		self.thread = _thread.start_new_thread(self.receive, ())
 		
 	def receive(self):
 		while True:
-			ret=str(self.Socker.recv(1024),encoding="utf-8")
-			print(self.name + " recv:" + ret)
+			ret=str(self.Socker.recv(1024), encoding="utf-8")
+			#print(self.name + " recv:" + ret)
 			messageList = ret.split(" ")
 			if messageList[0] == "NAME":
 				self.order = messageList[1]
@@ -48,6 +53,7 @@ class Player:
 				tmp = ret.split(" ")
 				for i in range(16):
 					self.state.append([float(tmp[1 + i*2]), float(tmp[2 + i*2])])
+					self.getNewState = True
 				
 			if messageList[0]=="MOTIONINFO":
 				self.x_coordinate = float(messageList[1])
@@ -59,6 +65,10 @@ class Player:
 				self.sweep = 0
 			if messageList[0]=="GO":
 				self.go = True
+			if messageList[0]=="SCORE":
+				self.score = int(messageList[1])
+			if messageList[0]=="GAMEOVER":
+				break
 				
 	def sendStrategy(self, shot, sweep = 0):
 		self.sweep = sweep
@@ -66,14 +76,21 @@ class Player:
 			time.sleep(0.5)
 		self.Socker.send(bytes("BESTSHOT " + list_to_str(shot), encoding="utf-8"))
 		self.go = False
+		self.getNewState = False
 	
 class Env:
+	observation_space = gym.spaces.box.Box(np.array([0] + [0]*32), np.array([15] + [(4.75 if (i&1)==0 else 11) for i in range(32)]))
+	action_space = gym.spaces.box.Box(np.array([2.5, -2.23, -10]), np.array([10, 2.23, 10]))
 	def Start(self):
 		self.shotNum = 0
+		self.reward = 0
 		#单击快速模式
 		if SLOW_MODE:
 			pyautogui.moveTo(CLICK_POSITION[0][0], CLICK_POSITION[0][1])
 			time.sleep(1)
+		time.sleep(1)
+		pyautogui.click(CLICK_POSITION[0][0], CLICK_POSITION[0][1])
+		time.sleep(0.3)
 		pyautogui.click(CLICK_POSITION[0][0], CLICK_POSITION[0][1])
 		
 		time.sleep(1)
@@ -83,6 +100,7 @@ class Env:
 		if SLOW_MODE:
 			time.sleep(1)
 		self.player.append(Player("Second"))
+		time.sleep(0.3)
 		
 		
 		#单击准备、开始对局
@@ -91,7 +109,7 @@ class Env:
 			time.sleep(1)
 		pyautogui.click(CLICK_POSITION[1][0], CLICK_POSITION[1][1])
 		
-		time.sleep(0.1)
+		time.sleep(0.3)
 		
 		if SLOW_MODE:
 			pyautogui.moveTo(CLICK_POSITION[2][0], CLICK_POSITION[2][1])
@@ -108,6 +126,44 @@ class Env:
 			pyautogui.moveTo(CLICK_POSITION[3][0], CLICK_POSITION[3][1])
 			time.sleep(1)
 		pyautogui.click(CLICK_POSITION[3][0], CLICK_POSITION[3][1])
+	
+	def GetPosition(self):
+		side = self.shotNum&1^1
+		while self.player[side].getNewState == False:
+			time.sleep(0.1)
+		position = self.player[side].state
+		return position
+	
+	def GetState(self):
+		state = np.array(self.GetPosition()).ravel()
+		return np.insert(state, 0, self.shotNum)
+	
+	def reset(self):
+		self.End()
+		self.Start()
+		return self.GetState()
+		
+	def GetReward(self):
+		side = (self.shotNum&1)^1
+		position = self.GetPosition()
+		#return np.sum(np.linalg.norm(position[side::2]) > 0.1) - np.sum(np.linalg.norm(position[side^1::2]) > 0.1)
+		if np.sum(np.linalg.norm(position) > 0.1) == 0:
+			return 0
+		dist = np.linalg.norm(position - np.array(TEE), axis = 1)
+		distPerSide = [dist[0::2], dist[1::2]]
+		if np.min(distPerSide[side]) < np.min(distPerSide[side^1]):
+			return np.sum(distPerSide[side] < np.min(distPerSide[side^1]))
+		else:
+			return np.sum(distPerSide[side^1] < np.min(distPerSide[side]))
+		
+		
+		
+	def step(self, action):
+		print(action)
+		self.reward -= self.GetReward()
+		self.GiveStrategy(action)
+		self.reward += self.GetReward()
+		return self.GetState(), self.reward, self.shotNum==16, ""
 		
 if __name__ == "__main__":
 	tmp = Env()
